89edc319775a0a165e084f1ac14d9458bf4458b6
Yūri Katsuki <yuri@katsuki.moe>
2023-06-13 03:20:35 -0400

    [FIX] use 2 width tab, 4 is evil :P

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Always prefer swc typescript compiler
+  swcMinify: true,
+
+  // Output everything to standalone files
+  output: 'standalone',
+
+  // https://hayao0819.com/twitter => https://twitter.com/hayao0819
+  async redirects() {
+    return [
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/hayao0819',
+        permanent: true,
+      }
+    ]
+  },
+};
+
+// Always use modules, commonjs is evil!
+export default nextConfig;
