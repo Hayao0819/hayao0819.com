3f6d2721cd154d2974fec6691fc44ca581ddd618
Yūri Katsuki <yuri@katsuki.moe>
2023-06-14 13:00:51 -0400

    [DMV] eslint && ts errors bypass :D

--- src/hooks/SideBar.tsx
+++ src/hooks/SideBar.tsx
@@ -2,5 +2,10 @@ import { createContext } from "react";
 
 export const sideBarContext = createContext({
     isOpened: false,
+    // If you want to shut up eslint errors for a while for type errors, use:
+    // @ts-ignore.
+
+    // If you want to shut up eslint errors for a while for type errors, use:
+    // eslint-disable-next-line
     setOpened: (isOpen: boolean) => {},
 });
