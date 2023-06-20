6c56b36508022dc2f97a361050d2ae9173a912fb
hayao <hayao@fascode.net>
2023-06-20 10:30:42 +0900

    Update: Use FontAwasomeIcon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,8 @@ import Footer from "@/components/Footer";
 //import { ReactNode } from "react";
 import { My } from "./Icons";
 import { Item as MenuItem } from "./Menu";
-import { faContactCard, faHouse, faKitchenSet, faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faContactCard, faHouse, faKitchenSet, faMessage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Props {
     children: React.ReactNode;
@@ -28,9 +29,7 @@ function HeaderForSP() {
         <div className="navbar static w-full bg-base-300 sm:hidden">
             <div className="flex-none">
                 <label htmlFor="sidebar" className="btn-ghost btn-square btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-6 w-6 stroke-current">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
+                    <FontAwesomeIcon icon={faBars} size="xl" />
                 </label>
             </div>
             <div className="mx-2 flex-1 px-2">Navbar Title</div>
@@ -66,9 +65,7 @@ function DrawerContents({ children }: Props) {
         <div className="drawer-content flex-col items-center justify-center sm:flex">
             <HeaderForSP />
             <main className="grow p-4 leading-8">{children}</main>
-            <div className="w-full">
-                <Footer />
-            </div>
+            <Footer />
         </div>
     );
 }
