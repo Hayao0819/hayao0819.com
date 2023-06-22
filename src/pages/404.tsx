861a7533d7267307ad363dd749ef9918f08b343f
hayao <hayao@fascode.net>
2023-06-22 15:46:25 +0900

    Update: gifをコンポーネント化

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,18 +3,17 @@ import Image from "next/image";
 import { ReactNode, useState } from "react";
 //import { useState } from "react";
 import useKonami from "use-konami";
+import React from "react";
 
 export default function NotFound() {
     return (
         <>
-            <div>
-                <PartyParrot>
-                    <H2>404 Not Found</H2>
-                </PartyParrot>
-                <PartyParrot>
-                    <p>内容が無いようです。</p>
-                </PartyParrot>
-            </div>
+            <PartyParrot>
+                <H2>404 Not Found</H2>
+            </PartyParrot>
+            <PartyParrot>
+                <p>内容が無いようです。</p>
+            </PartyParrot>
         </>
     );
 }
@@ -27,15 +26,19 @@ function PartyParrot({ children }: { children: ReactNode }) {
         },
     });
 
+    const GifImage = () => {
+        return <Image src="/emoji/partyparrot.gif" alt="PartyParrot" width={32} height={32} />;
+    };
+
     if (konami) {
         return (
             <div className="flex items-center">
-                <Image src="/emoji/partyparrot.gif" alt="PartyParrot" width={32} height={32} />
+                <GifImage />
                 {children}
-                <Image src="/emoji/partyparrot.gif" alt="PartyParrot" width={32} height={32} />
+                <GifImage />
             </div>
         );
-    }else{
+    } else {
         return <>{children}</>;
     }
 }
