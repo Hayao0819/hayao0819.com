35ed4056922cee9d3563acae15214e7ffb124067
hayao <hayao@fascode.net>
2023-06-29 13:50:08 +0900

    Update: Add neumorphism shadow

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -19,13 +19,13 @@ export function Item({ link, children, icon }: ItemProp): JSX.Element {
         iconElement = <FontAwesomeIcon className="" icon={icon} />;
     }
 
-    const defaultClassList = "m-4 flex items-center rounded-lg px-4 !bg-transparent py-2 text-sm";
+    const defaultClassList = "m-4 flex items-center rounded-lg px-4 !bg-transparent py-4 text-sm";
     const router = useRouter();
     const [classState, setClass] = useState(defaultClassList);
 
     useEffect(() => {
         if (removeHashFlag(router.asPath) == link) {
-            setClass(defaultClassList + " !neumo-float-shadow");
+            setClass(defaultClassList + " !neumo-pushed-shadow");
         } else {
             setClass(defaultClassList);
         }
