8f63c1370f07a4e01be02ea57e2697305f4d6c6a
hayao <hayao@fascode.net>
2023-07-02 00:04:40 +0900

    Update: 全体のデザインを変更

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -39,7 +39,7 @@ export function DrawerContents({ children }: { children: ReactNode }) {
     return (
         <div className="daisy-drawer-content flex min-h-screen flex-col items-center justify-center">
             <Header />
-            <main className="w-full grow p-4 leading-8">{children}</main>
+            <main className="neumo-pulled m-4 w-full grow p-4 leading-8">{children}</main>
             <Footer />
         </div>
     );
