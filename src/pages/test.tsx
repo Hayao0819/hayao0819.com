02f0aff76d677a0e470b4f5b0755a46c22c281ca
hayao <hayao@fascode.net>
2023-07-01 23:25:35 +0900

    Fix: Remove unused function

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,12 +3,6 @@ import { H2, H3 } from "@/components/Headlines";
 import ThemeButton from "@/components/ThemeBtn";
 
 export default function Test() {
-    const switchTheme = (
-        <input type="button" className="daisy-btn">
-            テーマを切り替える
-        </input>
-    );
-
     return (
         <div className="">
             <H2>テスト</H2>
