31d8cb7e7921d86d9b1a19b43fab1c7dc7aaccb6
hayao <hayao@fascode.net>
2023-07-13 14:09:12 +0900

    Fix: post link

--- src/components/layouts/Diary/Preview.tsx
+++ src/components/layouts/Diary/Preview.tsx
@@ -5,7 +5,7 @@ import { DiaryPreview as DiaryPreviewType } from "@/types/diaries";
 export default function DiaryPreview({ diaryPreview }: { diaryPreview: DiaryPreviewType }) {
     return (
         <div className="neumo-sink m-6 grow p-8">
-            <Link href={`/diary/${diaryPreview.slug}`}>
+            <Link href={`/diary/posts/${diaryPreview.slug}`}>
                 <h2 className="text-2xl font-bold">{diaryPreview.title}</h2>
                 <p>{diaryPreview.description}</p>
                 <div className="text-right">
