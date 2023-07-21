d686735cabd8bce83790b62cb29f9a5e2254ebce
hayao <hayao@fascode.net>
2023-07-22 08:48:57 +0900

    Update: Make file and directory

--- tools/newpost/newpost.go
+++ tools/newpost/newpost.go
@@ -1,6 +1,13 @@
 package newpost
 
 import (
+	"os"
+	"path"
+	"path/filepath"
+
+	//"strings"
+	"time"
+
 	"github.com/spf13/cobra"
 )
 
@@ -14,25 +21,51 @@ var Categories = []BlogCategory{
 
 func Cmd() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "newpost タイトル 説明",
-		Args:  cobra.RangeArgs(1, 2),
+		Use:   "newpost URL タイトル 説明",
+		Args:  cobra.RangeArgs(2, 3),
 		Short: "新しい記事を作成します",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fm := NewBlogFrontMatter(args[0], "")
-			if len(args) >= 2 {
-				fm.Description = args[1]
+			desc := ""
+			if len(args) >= 3 {
+				desc = os.Args[2]
 			}
-
-			//cmd.Println()
-			tp, err := fm.MakeTemplate()
+			path, err := MakePost(args[0], args[1], desc)
 			if err != nil {
 				return err
 			}
-			cmd.Println(string(tp))
+			if path != nil {
+				cmd.Println(*path)
+			}
 
 			return nil
 		},
 	}
 
 	return &cmd
+}
+
+func MakePost(url, title, desc string) (*string, error) {
+	fm := NewBlogFrontMatter(title, desc)
+
+	// Make template
+	tp, err := fm.MakeTemplate()
+	if err != nil {
+		return nil, err
+	}
+
+	// path
+	currnet_dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	path := path.Join(currnet_dir, "diaries", time.Now().Format("20060102"), url, "index.md")
+
+	// Make file
+	os.MkdirAll(filepath.Dir(path), os.FileMode(0750))
+	err = os.WriteFile(path, tp, os.FileMode(0640))
+	if err != nil {
+		return nil, err
+	}
+
+	return &path, nil
 }
