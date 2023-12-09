a077fecb018c393f8227845c184f815d886ac8ec
hayao <hayao@fascode.net>
2023-12-10 05:11:19 +0900

    Add: css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import "../style/global.css";
-import "../style/neumo.css";
 
-import { MDXProvider } from "@mdx-js/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { ThemeProvider } from "next-themes";
-import { ReactNode, useState } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
