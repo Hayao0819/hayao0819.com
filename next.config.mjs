083ba79d128ace9340c1c87573b3daedf0b8e14b
hayao <hayao@fascode.net>
2023-12-13 00:54:35 +0900

    Fix: Next build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Always prefer swc typescript compiler
     swcMinify: true,
-
-    // Output everything to standalone files
     output: "export",
-
-    // Configure pageExtensions to include md and mdx
+    trailingSlash: true,
     pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -64,7 +64,7 @@ export const getStaticProps: GetStaticProps<BlogTopProps> = async () => {
 
         returnProps.posts.push({
             file: file,
-            url: "blog/posts/" + blogtools.mdPathToURL(file),
+            url: "posts/" + blogtools.mdPathToURL(file),
             meta: meta,
             content: parsed.content,
         });
