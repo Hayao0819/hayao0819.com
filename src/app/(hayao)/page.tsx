7f7fd98a5d737506ed9f806cf77bf7453c95f0b0
山田ハヤオ <shun819.mail@gmail.com>
2023-12-23 20:24:14 +0900

    Move: links file

--- src/app/(hayao)/page.tsx
+++ src/app/(hayao)/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import Links from "../(hayao-top)/links";
+import Links from "./links";
 
 export default function Home() {
     return (
--- src/app/(hayao-top)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-import Layout from "@/components/layouts/Layout";
-
-export default function HayaoLayout({ children }: { children: ReactNode }) {
-    return <Layout className="h-screen">{children}</Layout>;
-}
