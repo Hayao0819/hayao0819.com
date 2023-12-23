1d4039582f85f5efb847cebf88dfcd4aeeed9511
山田ハヤオ <shun819.mail@gmail.com>
2023-12-23 16:50:13 +0900

    Move: Use hayao layouts

--- src/app/(hayao)/page.tsx
+++ src/app/(hayao)/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-import Links from "./links";
+import Links from "../(hayao-top)/links";
 
 export default function Home() {
     return (
-        <div className="mx-auto flex h-full  w-fit flex-col items-center justify-center child:m-2">
+        <div className="m-auto flex h-full  w-fit flex-col items-center justify-center child:m-2">
             <h1 className="text-5xl font-bold">山田ハヤオ</h1>
 
             <div className="flex w-full text-center child:child:mx-auto child:grow child:child:text-xl">
