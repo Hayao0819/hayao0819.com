cdad3f55a763ffc0642c264de4b20f4450e2e690
hayao <hayao@fascode.net>
2023-12-29 03:28:05 +0900

    Update: Prepare for postlist for dir

--- src/app/(hayao)/blog/posts/[...slug]/page.tsx
+++ src/app/(hayao)/blog/posts/[...slug]/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
+import path from "path";
+
 import { BlogHeading } from "@/components/elements/Heading";
+import ShareBtns from "@/components/elements/ShareBtns";
 import { getFetchedBlogPostList } from "@/lib/blog/post";
 import { findPostFromUrl } from "@/lib/markdown/fromurl";
+import { PostList } from "@/lib/markdown/postlist";
 import { recursivePath } from "@/lib/utils";
 
 export const generateStaticParams = async () => {
@@ -16,17 +21,38 @@ export const generateStaticParams = async () => {
     });
 };
 
-export default function PostPage({ params }: { params: { slug: string } }) {
-    const postData = findPostFromUrl(params.slug);
+export default function PostPage({ params }: { params: { slug: string[] } }) {
+    const postData = findPostFromUrl(params.slug.join("/"));
+    console.log(postData);
 
-    if (postData.isDir) {
-        return <div>404</div>;
+    if (postData.isDir === true) {
+        const posts = new PostList().fetch(path.join(process.cwd(), "posts", ...params.slug), {});
+        return (
+            <>
+                {posts.getPosts().map((p) => {
+                    return <div key={p.url}>{p.meta.title}</div>;
+                })}
+            </>
+        );
     } else {
         return (
-            <div className="">
+            <div className="mx-5 flex h-full flex-col">
                 <BlogHeading level={1}>{postData.post?.meta.title}</BlogHeading>
-                {postData.parsed}
+                <div className="grow">{postData.parsed}</div>
+                <ShareBtns text="hoge" url="hoge" />
             </div>
         );
     }
+}
+
+export async function generateMetadata({ params }: { params: { slug: string[] } }): Promise<Metadata> {
+    const postData = findPostFromUrl(params.slug.join("/"));
+    if (postData.isDir === true) {
+        return {
+            title: "Posts",
+        };
+    }
+    return {
+        title: postData.post?.meta.title,
+    };
 }
