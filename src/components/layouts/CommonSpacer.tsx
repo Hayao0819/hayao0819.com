064b0ecccfaf54c02b61b9e1f50b1c02cf5fd3be
hayao <hayao@fascode.net>
2023-12-28 17:17:41 +0900

    Fix: Classname typo

--- src/components/layouts/CommonSpacer.tsx
+++ src/components/layouts/CommonSpacer.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
 
 export default function CommonSpacer({ children, className }: { children: React.ReactNode; className?: string }) {
-    return <div className={classNames("mx-auto md:2/3 xl:w-4/5 p-5", className)}>{children}</div>;
+    return <div className={classNames("mx-auto w-4/5 p-5", className)}>{children}</div>;
 }
