3f2f3ee73843687da59922f3939c87342727d771
hayao <hayao@fascode.net>
2023-12-29 20:03:47 +0900

    Add: About me

---
+++ src/app/(hayao)/me/page.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "@/components/elements/Heading";
+import CommonSpacer from "@/components/layouts/CommonSpacer";
+
+export default function Me() {
+    <CommonSpacer>
+        <Heading level={1}>自己紹介</Heading>
+    </CommonSpacer>;
+}
