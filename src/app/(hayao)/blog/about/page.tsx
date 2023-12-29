6331c0c258715346392fd7b8f7f623074d296e1d
hayao <hayao@fascode.net>
2023-12-29 20:03:59 +0900

    Update: Add blog about

--- src/app/(hayao)/blog/about/page.tsx
+++ src/app/(hayao)/blog/about/page.tsx
@@ -1,3 +1,10 @@
+import { Heading } from "@/components/elements/Heading";
+
 export default function BlogAbout() {
-    return <div>ブログについて</div>;
+    return (
+        <>
+            <Heading level={1}>ハヤオについて</Heading>
+            <p>しがない理系もどき大学生</p>
+        </>
+    );
 }
