56407d2f7e1abac1347946efd7cbe2e1848c66da
山田ハヤオ <shun819.mail@gmail.com>
2024-01-06 21:24:05 +0900

    Add: Gyagu page

--- src/app/(hayao)/gyagu/page.tsx
+++ src/app/(hayao)/gyagu/page.tsx
@@ -1,10 +1,57 @@
+import { BlogHeading as Heading } from "@/components/elements/Heading";
+import Link from "@/components/elements/Link";
 import CommonSpacer from "@/components/layouts/CommonSpacer";
+import GyaguList from "@/components/layouts/GyaguList";
 
 export default function Gyagu() {
     return (
         <CommonSpacer>
-            <p>まだ内容がないようです。</p>
-            <p>イカしたコンテンツが追加されるんじゃないイカと思います。</p>
+            <div>
+                <div>
+                    <Heading level={3}>爆笑ギャグページ</Heading>
+                    <p>不足しています。助けてください。</p>
+                    <div className="">
+                        <GyaguList />
+                    </div>
+                </div>
+
+                <div>
+                    <Heading level={3}>ギャグが足りない人へ</Heading>
+                    <p>友達のサイトもうるサイトwww</p>
+                    <ul className="list-disc pl-10">
+                        <li>
+                            <Link href="https://sunset0916.net/gyagu/">サンセットのウェブサイト</Link>
+                        </li>
+                        <li>
+                            <Link href="https://midra.me/">Midraのサイト</Link>
+                        </li>
+                        <li>
+                            <Link href="https://lutica.net/gyagu.html">海老瀬 るちか</Link>
+                        </li>
+                        <li>
+                            <Link href="https://lunachi.me/gyagu/index.html">るなち</Link>
+                        </li>
+                        <li>
+                            <Link href="https://l1n4r1.art/gyagu/">L1n4r1Art</Link>
+                        </li>
+                        <li>
+                            <Link href="https://cffn.pw/r/gyagu">かふぇいんぱわぁ</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <Heading level={3}>ギャグが足りている方へ</Heading>
+                <p>こちらを御覧ください。</p>
+                <ul className="list-disc pl-10">
+                    <li>
+                        <Link href="https://souhait.me/gyagu">souhait.net</Link>
+                    </li>
+                </ul>
+
+                <b>連絡してくれれば載せるかもしれません</b>
+
+                <p className="text-center">これより下に内容は無いようです。</p>
+            </div>
         </CommonSpacer>
     );
 }
---
+++ src/components/layouts/GyaguList.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { ReactNode } from "react";
+
+export default function GyaguList() {
+    return (
+        <ul className="list-decimal pl-10">
+            <Gyagu>院試書かれた印紙。</Gyagu>
+            <Gyagu>首位のSyuilo</Gyagu>
+            <Gyagu>カニのカニバリズム</Gyagu>
+            <Gyagu>カニのカーニバル</Gyagu>
+            <div className="tooltip" data-tip="提供: リア友">
+                <Gyagu>つめあつめ</Gyagu>
+            </div>
+            <Gyagu>良いガタイとは言い難い</Gyagu>
+            <Gyagu>エーゲ海のええ外科医</Gyagu>
+            <div className="tooltip" data-tip="提供: 家族">
+                <Gyagu>「伊藤何等？」「1等」</Gyagu>
+            </div>
+            <Gyagu>みっかでみかん見っかった🍊</Gyagu>
+            <Gyagu tweet="インコの淫行">インコの淫行</Gyagu>
+            <Gyagu>以下活かしたイカ🦑</Gyagu>
+            <Gyagu>慶應をKO</Gyagu>
+            <Gyagu>会場を海上で開場！</Gyagu>
+            <Gyagu tweet="シンクに向かってrsync">
+                シンクに向かって<code>rsync</code>
+            </Gyagu>
+        </ul>
+    );
+}
+
+function Gyagu({ children, tweet }: { children: ReactNode | string; tweet?: string }) {
+    const openTweet = (text: string) => {
+        window.open(
+            "https://twitter.com/intent/tweet?text=ハヤオのサイトからギャグ↓" + encodeURIComponent("\n\n" + text),
+            "_blank",
+        );
+    };
+
+    if (tweet == undefined) {
+        if (typeof children == "string") {
+            tweet = children;
+        }
+    }
+
+    let onClickFunc: () => void;
+    let gyaguText: string;
+
+    if (tweet == undefined) {
+        onClickFunc = () => {
+            alert("このギャグはツイートできません。ハヤオにエラーを報告してください。");
+        };
+    } else {
+        gyaguText = tweet;
+        onClickFunc = () => openTweet(gyaguText);
+    }
+
+    return (
+        <li role="button" onClick={onClickFunc}>
+            {children}
+        </li>
+    );
+}
+
+/*
+<script>
+    // ツイート用リンクを作成
+    Array.from(document.getElementById("gyagu-list").children).forEach(e=>{
+        // カーソルをポインターに設定
+        e.setAttribute("role", "button");
+
+        // クリック時のイベントを設定
+        e.addEventListener("click", e=>{
+            //window.open("https://twitter.com/intent/tweet?text=ハヤオのサイトからギャグ↓" + encodeURIComponent("\n\n" + e.path[0].innerText), "_blank");
+            //console.log(e.path[0].innerText)
+            window.open("https://twitter.com/intent/tweet?text=ハヤオのサイトからギャグ↓" + encodeURIComponent("\n\n" + e.composedPath()[0].innerText), "_blank");
+            //console.log(e.composedPath()[0].innerText)
+        })
+    })
+</script>
+*/
