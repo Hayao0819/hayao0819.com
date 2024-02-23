364dde83ab807a7f26d301d6745fbd3e21dac26b
山田ハヤオ <shun819.mail@gmail.com>
2024-02-24 05:35:20 +0900

    Fix: Suspense error
    https://zenn.dev/sasatech/articles/8a1d0c23fb2b2f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/style/global.css";
 import "@/style/prism.css";
 
 import { Metadata as NextMetadata } from "next";
-import React, { ReactNode } from "react";
+import React, { ReactNode, Suspense } from "react";
 
 import GoogleAnalytics from "@/components/elements/GoogleAnalytics";
 import { genMetaData } from "@/lib/meta";
@@ -13,7 +13,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html className="overflow-x-scroll md:overflow-x-auto" lang="ja">
             <head>
-                <GoogleAnalytics />
+                <Suspense>
+                    <GoogleAnalytics />
+                </Suspense>
             </head>
             <body className="overflow-x-hidden overscroll-y-none">{children}</body>
         </html>
