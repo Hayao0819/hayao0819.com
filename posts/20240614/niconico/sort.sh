c49c134c2c6b7badafb0ac6d52f421b61d13fcdd
hayao <shun819.mail@gmail.com>
2024-06-14 17:35:22 +0900

    Update: Sort links

---
+++ posts/20240614/niconico/sort.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+current_dir=$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)
+
+linkfile="$current_dir/links.md"
+
+while read -r url; do
+    sed -E "s|- \[(.+)\]\((.+)\)|\1\n\2|g" "$linkfile" | grep -B 1 "$url" | head -n 1 | sed -e "s|^|- [|g" -e "s|$|]($url)|g"
+done < <(
+    sed -E "s|- \[(.+)\]\((.+)\)|\2|g" "$linkfile" | tr "/" " " | sed "s| sm| sm |g" | sort -n -k 5 | sed "s| sm | sm|g" | tr " " "/"
+) >> "$current_dir/index.md"
