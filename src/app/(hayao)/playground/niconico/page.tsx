87a9705dbaedb791916e91c281dcbb3c2bbede42
hayao <shun819.mail@gmail.com>
2024-06-15 09:13:44 +0900

    Fix: get info

--- src/app/(hayao)/playground/niconico/page.tsx
+++ src/app/(hayao)/playground/niconico/page.tsx
@@ -3,14 +3,17 @@
 import { useEffect, useState } from "react";
 
 export default function Niconico() {
-    const [info, setInfo] = useState({});
+    const [info, setInfo] = useState<string>();
 
     useEffect(() => {
         (async () => {
-            const data = await fetch("https://nicorekari.nanasi-rasi.net/DB.json", {
-                mode: "no-cors",
-            });
-            const json = await data.json();
+            const res = await fetch("https://nicorekari.nanasi-rasi.net/DB.json", {});
+            if (!res.ok) {
+                setInfo(res.statusText);
+                throw new Error(res.statusText);
+                return;
+            }
+            const json = await res.json();
             setInfo(json);
         })();
     }, []);
