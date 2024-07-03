165b11c6f89a471c443988a7b3415621d84699e0
hayao <shun819.mail@gmail.com>
2024-07-04 02:29:35 +0900

    Update: Remove ニコ動

--- src/app/(hayao)/page.tsx
+++ src/app/(hayao)/page.tsx
@@ -4,7 +4,7 @@ import Links from "./links";
 
 export default function Home() {
     return (
-        <div className="m-auto flex h-full  w-fit flex-col items-center justify-center child:m-2">
+        <div className="m-auto flex h-full w-fit  flex-col items-center justify-center gap-4 child:m-2">
             <h1 className="text-5xl font-bold">山田ハヤオ</h1>
 
             <div className="flex w-full text-center child:child:mx-auto child:grow child:child:text-xl">
@@ -14,9 +14,6 @@ export default function Home() {
             <div className="flex flex-col items-center gap-5">
                 <Link className="text-accent hover:underline" href="/me">
                     About Me
-                </Link>
-                <Link className="hover:underline" href="/playground/niconico/">
-                    ニコニコ動画検索
                 </Link>
 
                 <Link className="hover:underline" href="/blog">
