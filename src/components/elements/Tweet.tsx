42bdbcecab9ae73ecd0cf63044cabf27536e6ca0
hayao <shun819.mail@gmail.com>
2024-07-08 09:02:22 +0900

    Fix: Tweetwidget

--- src/components/elements/Tweet.tsx
+++ src/components/elements/Tweet.tsx
@@ -7,7 +7,7 @@ export type TweetProps = ComponentPropsWithoutRef<"div"> & LibTweetProps;
 export default function Tweet({ tweetId, options, onLoad, renderError, ...props }: TweetProps) {
     return (
         <div {...props}>
-            <TweetWidget tweetId={tweetId} options={options} onLoad={onLoad} renderError={renderError} />;
+            <TweetWidget tweetId={tweetId} options={options} onLoad={onLoad} renderError={renderError} />
         </div>
     );
 }
